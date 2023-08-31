2745c01c0c75483318754dace2a2e2e2e7bc45b5
KRATOS <samak519@student.liu.se>
2023-08-31 10:08:18 +0200

    Started work on lab1.

--- lab1/src/life.cpp
+++ lab1/src/life.cpp
@@ -3,13 +3,31 @@
 // TODO: remove this comment header
 
 #include <iostream>
-#include "grid.h"
-#include "lifeutil.h"
 #include <string>
+#include "lifeutil.h"
+#include "grid.h"
+
+using namespace std;
+
+void printWelcomeMessage() {
+    cout << "Welcome to the TDDD86 Game of Life,\n";
+    cout << "a simulation of the lifecycle of a bacteria colony.\n";
+    cout << "Cells (X) live and die by the following rules:\n";
+    cout << "- A cell with 1 or fewer neighbours dies.\n";
+    cout << "- Locations with 2 neighbours remain stable.\n";
+    cout << "- Locations with 3 neighbours will create life.\n";
+    cout << "- A cell with 4 or more neighbours dies.\n";
+}
+
+string promptForFileName(){
+    cout << "Grid input file name?" << endl;
+    string filename;
+    cin >> filename;
+    return filename;
+}
+
 int main() {
-
-    // TODO: Finish the program!
-
-    std::cout << "Have a nice Life! "  << std::endl;
+    printWelcomeMessage();
+    cout << "Have a nice Life! "  << endl;
     return 0;
 }
